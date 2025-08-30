b79bc6507f04fb20ceef9ca3fc300448f4079dce
Utkarsh <utkarsh@get1234.eu.org>
2025-08-30 17:22:19 +0530

    added oauth methods

--- real-estate-listing-website/app/(userauth)/api/auth/[...nextauth]/options.ts
+++ real-estate-listing-website/app/(userauth)/api/auth/[...nextauth]/options.ts
@@ -7,7 +7,6 @@ import { fetchData } from "../../../../../lib/api";
 const CrendentialsProviderOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      id: "credentialsprovider-1",
       credentials: {
         username: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
@@ -27,18 +26,28 @@ const CrendentialsProviderOptions: NextAuthOptions = {
             body: JSON.stringify(_currentUserDetails),
           });
           const user = await res.json();
-
+          console.log(user)
           if (res.ok && user) {
             return user;
           }
           throw new Error("User not found.");
         } catch (err) {
           console.log(err);
-          throw new Error("Something Went Wrong, Logs in console!");
+          
         }
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    }
+    )
   ],
+
   //pages: { // this is for custom pages
   //  signIn: "/sign-in"
   //}
